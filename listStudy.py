2c2ab181fe8f2021ca2eec9fcbfde093950a15a9
Marcelo Júnior <marcelohjw@gmail.com>
2021-04-19 08:32:33 -0300

    Comprehension stuff

--- listStudy.py
+++ listStudy.py
@@ -1,1 +1,11 @@
+import random
+
 print("Here i study list of comprehensions")
+
+print([x for x in [random.randint(1, 1001) for i in range(50)] if x % 9 == 0])
+
+multi_list = [[1, 2, 3],
+              [4, 5, 6],
+              [7, 99, 10]]
+
+print([col[1] for col in multi_list])
