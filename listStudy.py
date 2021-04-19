5b6ffa19257f51eb28b8ae22540b42846022624f
Marcelo Júnior <marcelohjw@gmail.com>
2021-04-19 08:41:48 -0300

    Finish lesson List Comprehensions

--- listStudy.py
+++ listStudy.py
@@ -9,3 +9,27 @@ multi_list = [[1, 2, 3],
               [7, 99, 10]]
 
 print([col[1] for col in multi_list])
+
+
+# Generator Stuff
+
+def is_prime(num):
+    for i in range(2, num):
+        if (num % i) == 0:
+            return False
+        return True
+
+
+def gen_prime(max_number):
+    for num1 in range(2, max_number):
+        if is_prime(num1):
+            yield num1
+
+
+prime = gen_prime(50)
+print("Prime:", next(prime))
+print("Prime:", next(prime))
+print("-----------------")
+doubles = (x * 2 for x in range(10))
+for i in doubles:
+    print("Double:", next(doubles))
