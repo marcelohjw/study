ef034d03ff5e525eea68f1e7206eec5945fb7bb2
Marcelo Júnior <marcelohjw@gmail.com>
2021-04-22 07:24:35 -0300

    Find using re module in a string, location and txt

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ all_apes = re.findall("ape", "The ape was in the apex")
 
 for i in all_apes:
     print(i)
+
+the_str = "The ape was in the apex"
+for i in re.finditer("ape.", the_str):
+    loc_tuple = i.span()
+    print(loc_tuple)
+    print(the_str[loc_tuple[0]:loc_tuple[1]])
