64116308ba1d32f3193d14fdb35d1e5aeca37afd
Marcelo Júnior <marcelohjw@gmail.com>
2021-04-23 08:45:42 -0300

    Multiple lines regex study

--- main.py
+++ main.py
@@ -18,3 +18,13 @@ print("Lesson 2 ")
 
 rand_str = "F.B.I. I.R.S. CIA"
 print("Matches :", len(re.findall(".\..\..", rand_str)))
+
+rand_str2 = """This is a 
+long string
+that goes
+on
+many lines"""
+print(rand_str2)
+regex = re.compile("\n")
+rand_str2 = regex.sub(" ", rand_str2)
+print(rand_str2)
