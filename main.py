47f16818107fd7ab9c80afd6420a43aea45dab67
Marcelo Júnior <marcelohjw@gmail.com>
2021-04-23 08:41:45 -0300

    Started lesson 2

--- main.py
+++ main.py
@@ -12,3 +12,9 @@ for i in re.finditer("ape.", the_str):
     loc_tuple = i.span()
     print(loc_tuple)
     print(the_str[loc_tuple[0]:loc_tuple[1]])
+
+print("=========================")
+print("Lesson 2 ")
+
+rand_str = "F.B.I. I.R.S. CIA"
+print("Matches :", len(re.findall(".\..\..", rand_str)))
