f72249c1ebd79fa33cead3d7ed6f8227a1a2a141
Marcelo Júnior <marcelohjw@gmail.com>
2021-12-14 06:29:40 -0300

    Python & SQlite study initiated

--- sqliteStudy/main.py
+++ sqliteStudy/main.py
@@ -1,29 +1,89 @@
-import random
-
-f_names = ["Michael", "Christopher", "Joshua", "Matthew", "David", "Daniel", "Andrew", "Joseph", "Justin",
-           "James", "Jessica", "Ashley", "Brittany", "Amanda", "Melissa", "Stephanie", "Jennifer",
-           "Samantha", "Sarah", "Megan", "Lauren", "Carol", "Marcelo"]
-
-l_names = ["Smith", "Johnson", "Williams", "Jones", "Brown", "Davis", "Miller", "Wilson", "Moore", "Taylor",
-           "Anderson", "Thomas", "Jackson", "White", "Harris", "Martin", "Thompson", "Garcia", "Martinez",
-           "Robinson", "Passos", "Camara"]
+import sqlite3
+import sys
+import csv
 
 
-def create_students(how_many):
-    for i in range(how_many):
-        f_name = f_names[random.randint(0, len(f_names) - 1)]
-        l_name = l_names[random.randint(0, len(l_names) - 1)]
+db_conn = sqlite3.connect('test.db')
+print("Database created")
+the_cursor = db_conn.cursor()
 
-        sex = random.choice(['M', 'F'])
-        print(f"INSERT INTO student (f_name, l_name, sex) VALUES ('{f_name}', '{l_name}', '{sex}');")
+def print_db():
+    try:
+        result = the_cursor.execute("SELECT id, f_name, l_name, age, address, salary, hire_data FROM employees")
+        for row in result:
+            print("id:", row[0])
+            print("f_name:", row[1])
+            print("l_name:", row[2])
+            print("age:", row[3])
+            print("address:", row[4])
+            print("salary:", row[5])
+            print("hire_data:", row[6])
+    except sqlite3.OperationalError:
+        print("The table doesn't exist")
+    except:
+        print("Couldn't retrieve the information")
+
+try:
+    db_conn.execute("""CREATE TABLE employees(ID INT PRIMARY KEY AUTOINCREMENT NOT NULL, f_name TEXT NOT NULL, l_name 
+                    TEXT NOT NULL, age INT NOT NULL, address TEXT, salary REAL, hire_data TEXT);""")
+    db_conn.commit()
+    print("Table created")
+except sqlite3.OperationalError:
+    print("Table couldn't be created")
 
 
-create_students(10)
 
-def create_tes_scores(num_tests, num_studs):
-    for i in range(1, num_tests + 1):
-        for j in range(1, num_studs + 1):
-            score = random.randrange(1, 25)
-            print(f"INSERT INTO test_score VALUES ({j}, {i}, {score});")
+db_conn.execute(""""INSERT INTO employees(f_name, l_name, age, address, salary, hire_data) VALUES ('Derek', 'Banas', 
+43, '123 Main St', 500000, date('now'));""")
+print("Employee Entered")
 
-create_tes_scores(4, 10)
+
+try:
+    db_conn.execute("UPDATE employees SET address = '121 Main St' WHERE ID = 1")
+    db_conn.commit()
+except sqlite3.OperationalError:
+    print("Database couldn't be updated")
+
+print_db()
+
+try:
+    db_conn.execute("DELETE FROM employees WHERE ID = 1")
+    db_conn.commit()
+except sqlite3.OperationalError:
+    print("Data couldn't be deleted")
+
+print_db()
+
+db_conn.rollback()
+
+print_db()
+
+try:
+    db_conn.execute("ALTER TABLE employees ADD COLUMN 'image' BLOB DEFAULT NULL")
+    db_conn.commit()
+except sqlite3.OperationalError:
+    print("Table couldn't be altered")
+
+the_cursor.execute("PRAGMA TABLE_INFO(employees)")
+row_names = [nameTuple[1] for nameTuple in the_cursor.fetchall()]
+print(row_names)
+
+the_cursor.execute('SELECT COUNT(*) FROM employees')
+num_of_rows = the_cursor.fetchall()
+print("Total Rows:", num_of_rows[0][0])
+
+the_cursor.execute("SELECT SQLITE_VERSION()")
+print("SQLITE_VERSION :", the_cursor.fetchone())
+
+with db_conn:
+    db_conn.row_factory = sqlite3.Row
+    the_cursor = db_conn.cursor()
+    the_cursor.execute("SELECT * FROM employees")
+    for row in rows:
+        print("{}{}".format(row["f_name"], row["l_name"]))
+
+with open('dump.sql', 'w') as f:
+    for line in db_conn.iterdump():
+        f.write("%s\n" % line)
+
+db_conn.close()
